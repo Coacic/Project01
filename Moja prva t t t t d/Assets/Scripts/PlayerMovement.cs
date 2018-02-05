cc25daf8b49b2601087eb76df29086da492793f0
Coacic <alexa952aca@gmail.com>
2018-02-05 21:21:27 +0100

    project01 -1

---
+++ Moja prva t t t t d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 25f;
+    public float sidewaysForce = 0f;
+
+    void FixedUpdate ()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
